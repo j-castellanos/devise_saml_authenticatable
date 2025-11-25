6825cea484c5f9850c397266d26a760f7361ef9b
jose <josec1003@gmail.com>
2025-11-25 15:52:34 -0500

    encrypted attributes fix

--- lib/devise_saml_authenticatable/default_idp_entity_id_reader.rb
+++ lib/devise_saml_authenticatable/default_idp_entity_id_reader.rb
@@ -8,11 +8,13 @@ module DeviseSamlAuthenticatable
           allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
         ).issuer
       elsif params[:SAMLResponse]
-        ::RubySaml::Response.new(
-          params[:SAMLResponse],
-          settings: Devise.saml_config,
-          allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
-        ).issuers.first
+        response_xml = RubySaml::XML::Decoder.decode_message(params[:SAMLResponse])
+        document = RubySaml::XML.safe_load_nokogiri(response_xml)
+        issuer_response_nodes = document.xpath(
+          "/p:Response/a:Issuer",
+          RubySaml::Response::SAML_NAMESPACES
+        )
+        issuer_response_nodes.map(&:text).reject(&:empty?).first
       end
     end
   end
